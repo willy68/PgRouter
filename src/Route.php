3d075fb77a9bebc1e171727dc9cb2e0c4ed1174a
William Lety <william.lety@gmail.com>
2023-02-17 20:39:03 +0100

    Add host and schemes to Route

--- src/Route.php
+++ src/Route.php
@@ -13,6 +13,17 @@ class Route extends \Mezzio\Router\Route implements RouteInterface, MiddlewareSt
 {
     use MiddlewareAwareStackTrait;
 
+    public const HTTP_SCHEME_ANY = null;
+
+    /** @var ?string */
+    protected $host;
+
+    /** @var ?int */
+    protected $port;
+
+    /** @var null|string[] Schemes allowed with this route */
+    protected $schemes;
+
     protected $callback;
 
     protected $group;
@@ -55,6 +66,70 @@ class Route extends \Mezzio\Router\Route implements RouteInterface, MiddlewareSt
             $this->group = $group;
         }
 
+        return $this;
+    }
+
+
+    public function getHost(): ?string
+    {
+        return $this->host;
+    }
+
+    public function getPort(): ?int
+    {
+        return $this->port;
+    }
+
+    /**
+     * get schemes array available for this route
+     *
+     * @return null|string[] Returns HTTP_SCHEME_ANY or string of allowed schemes.
+     */
+    public function getSchemes(): ?array
+    {
+        return $this->schemes;
+    }
+
+    /**
+     * Indicate whether the specified scheme is allowed by the route.
+     *
+     * @param string $method HTTP method to test.
+     */
+    public function allowsScheme(string $scheme): bool
+    {
+        $schemes = strtolower($scheme);
+        return $this->allowsAnyScheme() || in_array($schemes, $this->schemes, true);
+    }
+
+    /**
+     * Indicate whether any schemes is allowed by the route.
+     */
+    public function allowsAnyScheme(): bool
+    {
+        return $this->schemes === self::HTTP_SCHEME_ANY;
+    }
+
+    /**
+     * set schemes available for this route
+     *
+     * @return Route
+     */
+    public function setSchemes(?array $schemes = null): self
+    {
+        $schemes       = is_array($schemes) ? array_map('strtolower', $schemes) : $schemes;
+        $this->schemes = $schemes;
+        return $this;
+    }
+
+    public function setHost(string $host): self
+    {
+        $this->host = $host;
+        return $this;
+    }
+
+    public function setPort(int $port): self
+    {
+        $this->port = $port;
         return $this;
     }
 }
